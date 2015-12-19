1c82467db25763d8ab6715a9963e3876c73cfd86
Maxim <skvozik@gmail.com>
2015-12-19 23:24:21 +0600

    api_handler added

---
+++ WeatherAPI/api_handler.py
@@ -0,0 +1,13 @@
+import os
+import requests
+
+CLIENT_ID = os.environ['API_CLIENT_ID']
+CLIENT_SECRET = os.environ['API_SECRET']
+
+def get_weather(city, country):
+    message = requests.get('http://api.aerisapi.com/observations/{city},{country} \
+                           ?client_id=PjkW9QcuMXWbUqH02888X&client_secret=DLDt9gref9bjfkNcV6eDE1YVH6zXjeYRfwULshOw'
+                           .format(city=city, country=country))
+    print(message)
+
+get_weather('bishkek', 'kg')
