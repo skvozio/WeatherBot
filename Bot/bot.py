53c4ecba3f1f98fb72c18690055887076b097011
Maxim <skvozik@gmail.com>
2016-02-18 17:27:38 +0600

    Forecast API request added

--- Bot/bot.py
+++ Bot/bot.py
@@ -2,7 +2,7 @@ import os
 import requests
 import emoji
 
-from  WeatherAPI.api_handler import get_weather
+from WeatherAPI.api_handler import get_weather
 
 
 TOKEN = os.environ['TOKEN']
@@ -21,7 +21,8 @@ class Bot(object):
     def create_message(self, update):
         print ('create message', update)
         if 'text' in update['message'].keys():
-            if any(update['message']['text'].lower() in element for element in ['/help', emoji.emojize(':black_question_mark_ornament:help')]):
+            if any(update['message']['text'].lower() in element for element in
+                   ['/help', emoji.emojize(':black_question_mark_ornament:help')]):
                 text = 'To receive weather please send a message with city name.\nTo get help send me /help command'
             elif update['message']['text'].lower() == 'city':
                 text = 'Погода какого города вас интересует'
--- WeatherAPI/api_handler.py
+++ WeatherAPI/api_handler.py
@@ -1,21 +1,46 @@
-import os
+import os, datetime, emoji
 import requests
 
 CLIENT_ID = os.environ['API_CLIENT_ID']
+CURRENT_WEATHER_URI = 'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={ID}'
+FORECAST_URI = 'http://api.openweathermap.org/data/2.5/forecast/daily?q={city}&appid={ID}'
 print(CLIENT_ID)
 
+def emojize_condition(condition):
+    if condition.lower() == 'clear':
+        condition = emoji.emojize(':sunny:', use_aliases=True)
+    return condition
 
 def get_weather(city):
-    message = requests.get('http://api.openweathermap.org/data/2.5/weather?q={city}\
-                           &appid={ID}'
+    message = requests.get(CURRENT_WEATHER_URI
                            .format(city=city, ID=CLIENT_ID)).json()
     if 'message' in message.keys():
         return message['message']
-    return message['name'] +' '+ str(int(message['main']['temp'])-273) + '°C'
+    return message['name'] + ' ' + str(int(message['main']['temp']) - 273) + '°C'
+
+
+def get_forecast(city):
+    message = requests.get(FORECAST_URI.format(city=city, ID=CLIENT_ID)).json()
+
+    daily_temps = [(int(day_temp['temp']['max']-273), day_temp['dt'], day_temp['weather'][0]['main'])
+                   for day_temp in message['list'][:3]]
+    print(daily_temps)
+    response = 'Forecast for ' + message['city']['name'] + ':\n'
+    for (temperature, date, condition) in daily_temps:
+        date = datetime.datetime.fromtimestamp(date).strftime('%d.%m.%Y')
+        response += date + ': ' + str(temperature) + '°C '+ emojize_condition(condition) + '\n'
+
+    return response
 
 
 if __name__ == '__main__':
-    print(get_weather('Бишкек,кг'))
+    print(get_weather('Бишкек'))
+    print(get_forecast('Бишкек,кг'))
+    print(emoji.demojize('☀️'))
+    print()
+    # message = requests.get('https://facebook.com/')
+    # print(message.headers)
+    # print(message.cookies)
     update = {}
-    #if not update['message']:
-     #   print('ololol')
+    # if not update['message']:
+    #   print('ololol')
