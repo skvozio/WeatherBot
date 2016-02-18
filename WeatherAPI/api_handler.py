b2747ac0835cfe5a0988f7d7e4334143eb958dd6
Maxim <skvozik@gmail.com>
2016-02-19 00:39:17 +0600

    Some emoji tweaks

--- WeatherAPI/api_handler.py
+++ WeatherAPI/api_handler.py
@@ -4,12 +4,18 @@ import requests
 CLIENT_ID = os.environ['API_CLIENT_ID']
 CURRENT_WEATHER_URI = 'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={ID}'
 FORECAST_URI = 'http://api.openweathermap.org/data/2.5/forecast/daily?q={city}&appid={ID}'
-print(CLIENT_ID)
+EMOJI_WEATHER = {
+    'clear' : b'\xe2\x98\x80\xef\xb8\x8f'.decode(),
+    'rain' : ':cloud_with_rain:',
+    'snow' : b'\xe2\x9d\x84\xef\xb8\x8f'.decode()
+}
+
 
 def emojize_condition(condition):
-    if condition.lower() == 'clear':
-        condition = emoji.emojize(':sunny:', use_aliases=True)
-    return condition
+
+    if condition.lower() in EMOJI_WEATHER.keys():
+        condition = EMOJI_WEATHER[condition.lower()]
+    return emoji.emojize(condition, use_aliases=True)
 
 def get_weather(city):
     message = requests.get(CURRENT_WEATHER_URI
@@ -23,7 +29,7 @@ def get_forecast(city):
     message = requests.get(FORECAST_URI.format(city=city, ID=CLIENT_ID)).json()
 
     daily_temps = [(int(day_temp['temp']['max']-273), day_temp['dt'], day_temp['weather'][0]['main'])
-                   for day_temp in message['list'][:3]]
+                   for day_temp in message['list'][:7]]
     print(daily_temps)
     response = 'Forecast for ' + message['city']['name'] + ':\n'
     for (temperature, date, condition) in daily_temps:
