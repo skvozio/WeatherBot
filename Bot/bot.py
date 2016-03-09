148015f467060383301496fb6bc30bb751b52242
Maxim <skvozik@gmail.com>
2016-03-09 17:19:55 +0600

    Database setup

--- Bot/bot.py
+++ Bot/bot.py
@@ -33,8 +33,8 @@ class Bot(object):
         )
         self.cursor = self.conn.cursor()
 
-        self.cursor.close()
-        self.conn.close()
+        #self.cursor.close()
+        #self.conn.close()
     
     def _post_method(self, method, data):
         response = requests.post(BASE_URL+method, json=data)
